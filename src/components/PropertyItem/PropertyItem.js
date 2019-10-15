9a977cb7d8125422d3dfea1f3bfead42bf44b53e
Marian <marianmrva123@gmail.com>
2019-10-15 22:18:34 +0200

    Update static data with api data

--- src/components/PropertyItem/PropertyItem.js
+++ src/components/PropertyItem/PropertyItem.js
@@ -7,27 +7,29 @@ import Truncate from 'react-truncate';
 
 const PropertyItem = (props) => {
     return (
-        <a href="#" target="_blank" rel="noopener noreferrer">
-            <Card className="shadow">                
-                <CardImg top width="100%" src="https://res.cloudinary.com/emeraldstay/image/upload/f_auto,g_auto,t_search_page/nsqurylx6k7gxfrnbcwk" alt="Card image cap" />
-                <CardBody>
-                    <CardTitle>
-                        <Truncate lines={1} ellipsis={<span>...</span>}>
-                            ABACHI - Luxury ski chalet
-                        </Truncate>
-                    </CardTitle>
-                    <CardSubtitle className="my-1">
-                        Les Gets
-                    </CardSubtitle>
-                    <CardSubtitle className="my-1">
-                        15 Adults beds, 7 Bedrooms
-                    </CardSubtitle>                    
-                    <CardSubtitle className="my-1">
-                        from €525 / night
-                    </CardSubtitle>                    
-                </CardBody>
+        
+            <Card className="shadow">
+                <a href={`https://emeraldstay.com/property/${props.property.slug}`} target="_blank" rel="noopener noreferrer">
+                    <CardImg top width="100%" src={props.property.twitter_cards_image_cache} alt="Card image cap" />
+                    <CardBody>
+                        <CardTitle>
+                            <Truncate lines={1} ellipsis={<span>...</span>}>                            
+                                {props.property.title}
+                            </Truncate>
+                        </CardTitle>
+                        <CardSubtitle className="my-1">
+                            {props.property.city}
+                        </CardSubtitle>
+                        <CardSubtitle className="my-1">
+                            {props.property.beds_adults} Adults beds, {props.property.number_of_bedrooms} Bedrooms
+                        </CardSubtitle>                    
+                        <CardSubtitle className="my-1">
+                            from €{props.property.property_price} / night
+                        </CardSubtitle>                    
+                    </CardBody>
+                </a>
             </Card>
-        </a>
+        
     );
 }
 
--- src/components/PropertyList/PropertyList.js
+++ src/components/PropertyList/PropertyList.js
@@ -3,20 +3,34 @@ import { connect } from "react-redux";
 import PropertyItem from "../PropertyItem/PropertyItem";
 import { fetchMoreProperties } from "../../redux/propertyListActions"
 
-class PropertyList extends React.Component {
+class PropertyList extends React.Component {    
     componentDidMount() {
         this.props.fetchMoreProperties()
     }
     render() {
-        return (
-            <PropertyItem/>
-        )
+        if(this.props.fetchedProperties) {
+            return (
+                <div className="row">
+                    {this.props.fetchedProperties.map((property, i) => (
+                        <div className="col-12 col-sm-6 col-md-4 col-lg-4 col-xxl-3 mt-4" key={i}>
+                            <PropertyItem property={property} key={i}/>
+                        </div>
+                    ))}
+                </div>
+            )
+        }
+        else {
+            return <p>loading...</p>
+        }
     }
 }
+
 
 const mapDispatchToProps = (dispatch) => ({
     fetchMoreProperties: () => dispatch(fetchMoreProperties())
 })
-const mapStateToProps = state => ({})
+const mapStateToProps = state => ({
+    fetchedProperties: state.propertyListReducer.fetchedProperties
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(PropertyList);
