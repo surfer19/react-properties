1c3b2db2487c8a3541c1363591f13ebe426eaab8
Marian <marianmrva123@gmail.com>
2019-10-15 15:27:31 +0200

    Add dependencies sceleton

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container-fluid">
+        <h2 className="text-center">HI!</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
